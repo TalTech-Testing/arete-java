c5e0fe771d010e388980dd788da53ef73f848ce3
envomp <envomp@ttu.ee>
2020-11-04 15:27:16 +0200

    add DockerParameters

---
+++ src/main/java/ee/taltech/arete/java/request/tester/DockerParameters.java
@@ -0,0 +1,31 @@
+package ee.taltech.arete.java.request.tester;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class DockerParameters {
+	private final String uniid;
+
+	@Builder.Default
+	private final Set<String> systemExtra = new HashSet<>();
+
+	@Builder.Default
+	private final Integer timeout = 120;
+
+	private final String commitMessage;
+
+	@Builder.Default
+	private final String contentRoot = "/student";
+
+	private final String extra;
+
+	@Builder.Default
+	private final String testRoot = "/tester";
+}
